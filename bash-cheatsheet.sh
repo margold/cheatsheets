4d297a9995358728d28dd435d0a2f106db1039a2
margold <irina@zenguard.org>
2017-02-18 17:47:08 +0000

    Update bash-cheatsheet.sh

--- bash-cheatsheet.sh
+++ bash-cheatsheet.sh
@@ -19,3 +19,25 @@ cat test.txt | tr -d [:space:]
 export strace-out=strace.out
 strace -v -f -e trace=network $command &> $strace-out
 cat $strace-out | grep 'connect(' | grep INET
+
+
+### debug connection issues
+curl -v $URL
+
+# if curl fails with SSL problem
+openssl s_client -msg -connect $HOST:$PORT
+
+# -i (select all internet network files), -n (don't convert network numbers to names)
+lsof -i -n
+
+# select all internet files that belong to processes owned by user `margold`
+# -a (AND the list options instead of ORing them)
+lsof -ai -umargold -n
+
+# --inet and --inet6 (address families)
+# -n (numeric), -p (pid and name of program to which the socket belongs)
+netstat --inet --inet6 -np
+
+# -t (tcp), -u (udp), -n (numeric), -a (both listening and non-listening sockets)
+# -p (pid and name of program to which the socket belongs), -l (show only listening sockets)
+netstat -tunapl
